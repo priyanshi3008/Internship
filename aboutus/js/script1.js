a58b1be5f0a2df3feff39689850df8c1f986932a
Priyanshi Agrawal <44469226+priyanshi3008@users.noreply.github.com>
2020-07-19 02:47:37 +0530

    Add files via upload

---
+++ aboutus/js/script1.js
@@ -0,0 +1,31 @@
+const prev  = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+const track = document.querySelector('.track');
+
+let carouselWidth = document.querySelector('.carousel-container').offsetWidth;
+
+window.addEventListener('resize', () => {
+  carouselWidth = document.querySelector('.carousel-container').offsetWidth;
+})
+
+let index = 0;
+
+next.addEventListener('click', () => {
+  index++;
+  prev.classList.add('show');
+  track.style.transform = `translateX(-${index * carouselWidth/4}px)`;
+
+  if (track.offsetWidth - (index * carouselWidth/4) < carouselWidth) {
+    next.classList.add('hide');
+  }
+})
+
+prev.addEventListener('click', () => {
+  index--;
+  next.classList.remove('hide');
+  if (index === 0) {
+    prev.classList.remove('show');
+  }
+  track.style.transform = `translateX(-${index * carouselWidth/4}px)`;
+})
